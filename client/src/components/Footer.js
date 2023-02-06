90093b0bb7e2b382687ef4a7c98469728d86394b
mckk84 <mckk84@users.noreply.github.com>
2023-02-06 17:10:11 +0530

    client

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="container" style={{ padding:"10px 10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <p>@ Copyright. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
