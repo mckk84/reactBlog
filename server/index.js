8f97c60b2d3dcfcdda8e8ab9041485e4b8f72b56
mckk84 <mckk84@users.noreply.github.com>
2023-02-07 16:00:19 +0530

    fakedata

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express();
 const cors = require("cors");
-const { faker } = require('@faker-js/faker');
 
 require("dotenv").config();
 app.use(express.static('public'))
@@ -14,44 +13,14 @@ app.use(express.urlencoded({
     extended: true,
   })
 );
-
-const db = require('./modules/db');
-const Blogs = require('./models/Blogs');
-const User = require('./models/User');
-
-async function init()
-{
-    try{
-        console.log('init');
-        let blogs = await Blogs.find({});
-        let users = await User.find({});
-        console.log(users.length);
-        console.log(blogs.length);
-        for(i = 0; i < 10; i++){
-            let title = faker.name.jobTitle();
-            let title_description = faker.name.jobType();
-            let content = faker.lorem.paragraphs(5); 
-            let category = faker.name.jobArea();
-            let views = faker.random.numeric();
-            let index = views > 10 ? 1 : views;
-            let owner = ( typeof users[index] == 'undefined') ? users[1] : users[index];
-            const product = await Blogs.create({title, title_description, content, category, views, owner});
-            console.log(product);
-        }
-    } 
-    catch(e)
-    {
-        console.log(e);
-    }
-}
-//init();
+require('./modules/fakedata');
 
 app.get("/", async (req, res) => {
     res.send("API FOR BLOGGER");
 });
 
 app.post("/login", async (req, res) => {
-        
+    console.log(req.body);
     res.json({
         loggedIn:true,
         name:"Charan Kumar",
