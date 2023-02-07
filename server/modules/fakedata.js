111686ffce5968dde53e85a1367035ffe341c85e
mckk84 <mckk84@users.noreply.github.com>
2023-02-07 16:03:39 +0530

    Updated fakedata.js

--- server/modules/fakedata.js
+++ server/modules/fakedata.js
@@ -1,37 +1,45 @@
 const { faker } = require('@faker-js/faker');
 
-const db = require('./modules/db');
-const Blogs = require('./models/Blogs');
-const User = require('./models/User');
+const db = require('./db');
+const Blogs = require('../models/Blogs');
+const User = require('../models/User');
 const num_users = 5;
 const num_blogs = 10;
 
-let blogs = await Blogs.find({});
-let users = await User.find({});
-if( users.length == 0 )
+async function init_users()
 {
-    for(i = 0; i < num_users; i++)
+    let users = await User.find({});
+    if( users.length == 0 )
     {
-        let name = faker.name.fullName();
-        let email = faker.internet.email();
-        let password = "@#User123";
-        let isAdmin = 0;
-        const user = await User.create({name, email, password, isAdmin});
-    }    
-}
-
-if( blogs.length == 0)
-{
-    users = await User.find({});
-    for(i = 0; i < num_blogs; i++)
-    {
-        let title = faker.name.jobTitle();
-        let title_description = faker.name.jobType();
-        let content = faker.lorem.paragraphs(5); 
-        let category = faker.name.jobArea();
-        let views = faker.random.numeric();
-        let index = views > 10 ? 1 : views;
-        let owner = ( typeof users[index] == 'undefined') ? users[0] : users[index];
-        const product = await Blogs.create({title, title_description, content, category, views, owner});
+        for(i = 0; i < num_users; i++)
+        {
+            let name = faker.name.fullName();
+            let email = faker.internet.email();
+            let password = "@#User123";
+            let isAdmin = 0;
+            const user = await User.create({name, email, password, isAdmin});
+        }    
     }
 }
+async function init_blogs()
+{
+    let blogs = await Blogs.find({});
+    if( blogs.length == 0)
+    {
+        users = await User.find({});
+        for(i = 0; i < num_blogs; i++)
+        {
+            let title = faker.name.jobTitle();
+            let title_description = faker.name.jobType();
+            let content = faker.lorem.paragraphs(5); 
+            let category = faker.name.jobArea();
+            let views = faker.random.numeric();
+            let index = views > 10 ? 1 : views;
+            let owner = ( typeof users[index] == 'undefined') ? users[0] : users[index];
+            const product = await Blogs.create({title, title_description, content, category, views, owner});
+        }
+    }
+}
+
+init_users();
+init_blogs();
